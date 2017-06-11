d5ce1b94002b5d168a1ede7666f387a6cfd0014b
kristaprokop <krista.prokop@gmail.com>
2017-06-10 23:35:33 -0500

    Create form classes and routes

--- apps/register/forms.py
+++ apps/register/forms.py
@@ -1,7 +1,10 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
 
-from .models import User, CreditCard
+from .models import Users, CreditCard #CustomUser
 
+import re
 
 def MainForm(field_list, *args, **kwargs):
     class MainForm(forms.ModelForm):
@@ -9,11 +12,40 @@ def MainForm(field_list, *args, **kwargs):
             password = forms.CharField(widget=forms.PasswordInput())
 
         class Meta:
-            model = User
+            model = Users
             fields = field_list
 
         def __init__(self):
             super(MainForm, self).__init__(*args, **kwargs)
+
+        def clean(self):
+            cleaned_data = self.cleaned_data
+            
+            #validate email address
+            if 'email' in field_list:
+                email_regex = re.compile(r'^[a-zA-Z0-9\.\+_-]+@[a-zA-Z0-9\._-]+\.[a-zA-Z]*$')
+                email = cleaned_data.get("email")
+                if not email_regex.match(email):
+                    self.add_error('email', "Invalid email address" )
+
+            #validate names
+            if 'first_name' in field_list:
+                first_name = cleaned_data.get('first_name')
+                if len(first_name) < 2:
+                    self.add_error('first_name', "Name must be 2 or more characters" )
+
+            if 'last_name' in field_list:
+                last_name = cleaned_data.get('last_name')
+                if len(last_name) < 2:
+                    self.add_error('last_name', "Name must be 2 or more characters" )
+            
+            if 'password' in field_list:
+                password = cleaned_data.get('password')         
+                if len(password) < 8:
+                    self.add_error('password', "Password must be 8 or more characters" )
+
+               
+            return self.cleaned_data
 
     return MainForm()
 
@@ -28,3 +60,8 @@ def CreditCardForm(field_list, *args, **kwargs):
             super(CreditCardForm, self).__init__(*args, **kwargs)
     
     return CreditCardForm() 
+
+# class CustomUserForm(forms.ModelForm):
+#     class Meta:
+#         model = CustomUser
+#         fields = ['test']
--- apps/register/views.py
+++ apps/register/views.py
@@ -2,8 +2,8 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, redirect
-from .models import User
-from .forms import MainForm, CreditCardForm
+from .models import Users #CustomUser
+from .forms import MainForm, CreditCardForm #CustomUserForm
 
 #formdict.py contains lookup values for form fields and urls
 from .formdict import FORMDICT, URLSDICT
@@ -12,6 +12,11 @@ from .formdict import FORMDICT, URLSDICT
 def index(request):
     return render(request, 'register/index.html')
 
+def testing(request):
+    form = CustomUserForm(request.POST)
+    if form.is_valid():
+        form.save()
+    return redirect('/')
 
 def modal(request, string):
     path = request.path
@@ -21,16 +26,16 @@ def modal(request, string):
         lookup_path = URLSDICT[path]
     else:
         lookup_path = FORMDICT['default']
-
-    #instantiate user form with corresponding fields
+    
+    #get user fields for form
     user_fields = tuple(lookup_path['user_fields'])
     request.session['user_fields'] = user_fields
   
-    #instantiate credit card form with corresponding fields
+    #get cc fields for form
     credit_card_fields = tuple(lookup_path['credit_card_fields'])
     request.session['credit_card_fields'] = credit_card_fields
     
-    #pass both forms to template & combine into one form div
+    #instantiate forms and send to template to combine into one div
     context = {
         'user_form': MainForm(user_fields),
         'credit_card_form': CreditCardForm(credit_card_fields),
@@ -46,13 +51,14 @@ def register(request):
         credit_card_fields = request.session['credit_card_fields']
 
         user_form = MainForm(tuple(user_fields), request.POST)
+        credit_card_form = CreditCardForm(tuple(credit_card_fields), request.POST)
+
 
         if user_form.is_valid():
-            print "Success user!"
+            print "saved"
         else:
             print "user form problem"
-
-        credit_card_form = CreditCardForm(tuple(credit_card_fields), request.POST)
+            return redirect('/')
 
         if credit_card_form.is_valid():
             print "Success credit card!"
@@ -61,5 +67,43 @@ def register(request):
 
         return redirect('/')
 
+def myView(request, **string_path):
+    path = request.path
 
+    referer = request.META.get('HTTP_REFERER')
+    
+    #If path is not in the URLSDICT, use 'default' settings in FORMSDICT
+    if path in URLSDICT:
+        lookup_path = URLSDICT[path]
+    else:
+        lookup_path = FORMDICT['default']
+    
+    #get user fields for form
+    user_fields = tuple(lookup_path['user_fields'])
+    request.session['user_fields'] = user_fields
+  
+    #get cc fields for form
+    credit_card_fields = tuple(lookup_path['credit_card_fields'])
+    request.session['credit_card_fields'] = credit_card_fields
+
+    user_form = MainForm(user_fields, request.POST or None)
+    credit_card_form = CreditCardForm(credit_card_fields, request.POST or None)
+
+    
+    if request.method == 'POST':
+        if user_form.is_valid() and credit_card_form.is_valid():
+            user_form.save()
+            credit_card_form.save()
+            print "saved"
+          
+
+    context = {
+        'user_form': user_form,
+        'credit_card_form': credit_card_form,
+        'heading': lookup_path['heading'],
+        'subheading': lookup_path['subheading'],
+        'string_path': string_path['string_path'],
+    }
+
+    return render(request, 'register/modal.html', context)
 
